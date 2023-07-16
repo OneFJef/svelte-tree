05c85a83b6168426ce47d4ce1b58cc837e65dff3
Jef Mitchell <etches.aquatic_0x@icloud.com>
2023-07-16 16:21:14 -0500

    Removed typo.

--- src/lib/compnents/AuthCheck.svelte
+++ src/lib/compnents/AuthCheck.svelte
@@ -6,7 +6,7 @@
   <slot />
 {:else}
   <p class="text-error">
-    You must be signed in to view this page.AnimatedRoute
+    You must be signed in to view this page.
     <a class="btn btn-primary" href="/login">Sign in</a>
   </p>
 {/if}
